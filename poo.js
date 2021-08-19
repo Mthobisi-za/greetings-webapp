45bd8d95a5ccf381cb775c52371852991a9c5b3b
Mthobisi-za <mthobisimtho333@gmail.com>
2021-08-19 23:52:30 +0200

    changes

--- poo.js
+++ poo.js
@@ -2,16 +2,13 @@ const { Pool } = require("pg");
 
 var obj = { user: "mtho", password: "mthobisi", host: "localhost", port: 3009, database: "postgres" }
 ////postgres://YourUserName:YourPassword@YourHostname:5432/YourDatabaseName"
-
+var actual = process.env.DATABASE_URL;
+var password = process.env.Pass
 //var obj = "postgres://mtho:mthobisi@localhost:3009/postgres"
 var op = {database : process.env.DATABASE_URL, password: process.env.Pass}
-var check =  {database : process.env.DATABASE_URL }
-var connectStr ;
-if(process.env.DATABASE_URL){
-    connectStr = op
-} else{
-    connectStr = obj
-};
+var check =  {actual, password} || obj
+var connectStr = check ;
+
 
 module.exports = connectStr;
 
