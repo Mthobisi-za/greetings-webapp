278afa607ed149f6b50869eadb77fac628bbabf9
Mthobisi-za <mthobisimtho333@gmail.com>
2021-08-20 16:01:52 +0200

    changed port

--- poo.js
+++ poo.js
@@ -1,10 +1,10 @@
 const { Pool } = require("pg");
 
-var obj = { user: "mtho", password: "mthobisi", host: "localhost", port: 3009, database: "postgres" }
+var obj = { user: "mtho", password: "mthobisi", host: "localhost", port: 5432, database: "postgres" }
 ////postgres://YourUserName:YourPassword@YourHostname:5432/YourDatabaseName"
 
 //postgres://disqozuiackaiy:04869c9482b9ac085d31d382837d6130328f3d02e725aa9f16acbe6a9d8ec775@ec2-54-236-137-173.compute-1.amazonaws.com:5432/da3rds8i8jf24v
-var db = process.env.DATABASE_URL ||  "postgresql://mtho:mthobisi@localhost:3009/postgres";
+var db = process.env.DATABASE_URL ||  "postgresql://mtho:mthobisi@localhost:5432/postgres";
 var actual = process.env.DATABASE_URL;
 var deploy = {
     user:'disqozuiackaiy',
@@ -22,5 +22,5 @@ if(process.env.DATABASE_URL){
 }
 */
 
-module.exports = connectStr;
+module.exports = obj;
 
