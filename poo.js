b2ce797139dad857a471b8bd48384ea7fbdf1b49
Mthobisi-za <mthobisimtho333@gmail.com>
2021-08-20 12:24:05 +0200

    UPDATED

--- poo.js
+++ poo.js
@@ -4,7 +4,7 @@ var obj = { user: "mtho", password: "mthobisi", host: "localhost", port: 3009, d
 ////postgres://YourUserName:YourPassword@YourHostname:5432/YourDatabaseName"
 
 //postgres://disqozuiackaiy:04869c9482b9ac085d31d382837d6130328f3d02e725aa9f16acbe6a9d8ec775@ec2-54-236-137-173.compute-1.amazonaws.com:5432/da3rds8i8jf24v
-var db = process.env.DATABASR_URL ||  obj;
+var db = process.env.DATABASE_URL ||  obj;
 var deploy = {
     user:'disqozuiackaiy',
     password:"04869c9482b9ac085d31d382837d6130328f3d02e725aa9f16acbe6a9d8ec775",
