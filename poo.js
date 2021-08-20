426b901fcbe5583cc5ad1a796c8d75dc5ff2dc53
Mthobisi-za <mthobisimtho333@gmail.com>
2021-08-20 12:54:52 +0200

    updated

--- poo.js
+++ poo.js
@@ -5,14 +5,15 @@ var obj = { user: "mtho", password: "mthobisi", host: "localhost", port: 3009, d
 
 //postgres://disqozuiackaiy:04869c9482b9ac085d31d382837d6130328f3d02e725aa9f16acbe6a9d8ec775@ec2-54-236-137-173.compute-1.amazonaws.com:5432/da3rds8i8jf24v
 var db = process.env.DATABASE_URL ||  obj;
+
 var deploy = {
     user:'disqozuiackaiy',
     password:"04869c9482b9ac085d31d382837d6130328f3d02e725aa9f16acbe6a9d8ec775",
     host: "ec2-54-236-137-173.compute-1.amazonaws.com",
     database: "da3rds8i8jf24v",
     port: "5432"
-}
-var connectStr = deploy
+} 
+var connectStr = deploy;
 /*
 if(process.env.DATABASE_URL){
     connectStr = {db}
@@ -20,7 +21,6 @@ if(process.env.DATABASE_URL){
     connectStr = db
 }
 */
-
 
 module.exports = connectStr;
 
