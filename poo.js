831dd59fd205aa89243f2f63c28bc48d22906868
Mthobisi-za <mthobisimtho333@gmail.com>
2021-08-20 12:31:05 +0200

    updated

--- poo.js
+++ poo.js
@@ -1,5 +1,5 @@
 const { Pool } = require("pg");
-var connectStr 
+
 var obj = { user: "mtho", password: "mthobisi", host: "localhost", port: 3009, database: "postgres" }
 ////postgres://YourUserName:YourPassword@YourHostname:5432/YourDatabaseName"
 
@@ -9,13 +9,17 @@ var deploy = {
     user:'disqozuiackaiy',
     password:"04869c9482b9ac085d31d382837d6130328f3d02e725aa9f16acbe6a9d8ec775",
     host: "ec2-54-236-137-173.compute-1.amazonaws.com",
-    database: "da3rds8i8jf24v"
+    database: "da3rds8i8jf24v",
+    port: "5432"
 }
+var connectStr = deploy
+/*
 if(process.env.DATABASE_URL){
     connectStr = {db}
 } else{
     connectStr = db
 }
+*/
 
 
 module.exports = connectStr;
